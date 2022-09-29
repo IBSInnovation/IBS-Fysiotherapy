6c0423c88e1bf30b5fab3c7cbd980904c429d72d
nelisriebezos <niels@riezebos.net>
2022-09-29 11:27:25 +0200

    Springboot added
    I added some different springboot dependencies. I took them from the tennis application.
    I also renamed the main method.

---
+++ src/main/java/org/ibs/IBSApplication.java
@@ -0,0 +1,13 @@
+package org.ibs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IBSApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(IBSApplication.class, args);
+    }
+}
