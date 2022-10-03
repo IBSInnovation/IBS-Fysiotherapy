d53c7d1890eb50f1294e61afc563b6f3ce402eac
nelisriebezos <73901597+nelisriebezos@users.noreply.github.com>
2022-10-03 12:34:34 +0200

    Category service documentation
    I added some documentation for the category service

--- src/main/java/org/ibs/application/service/CategoryService.java
+++ src/main/java/org/ibs/application/service/CategoryService.java
@@ -9,15 +9,18 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 
-// tests nog niet gwschreven omdat ik niet zeker weet of we ook zo de try catch
-// willen uitvoeren.
-
 @Service
 @Transactional
 @AllArgsConstructor
 public class CategoryService implements ICategoryService {
     private final CategoryRepository categoryRepository;
 
+    /**
+     * Searches the database for a Category entity with the given id and returns it if it exists.
+     * @param id
+     * @return Category of given id
+     * @throws Exception
+     */
     @Override
     public Category getById(long id) throws Exception {
         try {
@@ -29,6 +32,11 @@ public class CategoryService implements ICategoryService {
 
     }
 
+    /**
+     * Searches the database for all Category entities and returns them.
+     * @return List of Category entities
+     * @throws Exception
+     */
     @Override
     public List<Category> getAll() throws Exception {
         try {
@@ -38,6 +46,12 @@ public class CategoryService implements ICategoryService {
         }
     }
 
+    /**
+     * Saves and updates the given Category entity in the database.
+     * @param category
+     * @return The saved Category entity
+     * @throws Exception
+     */
     @Override
     public Category persistCategory(Category category) throws Exception {
         try {
@@ -48,6 +62,12 @@ public class CategoryService implements ICategoryService {
         }
     }
 
+    /**
+     * Deletes the Category entity with the given id.
+     * @param id
+     * @return true if the operation succeeded
+     * @throws Exception
+     */
     @Override
     public boolean deleteCategory(long id) throws Exception {
         try {
