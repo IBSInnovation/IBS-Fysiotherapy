3b642549d9669b7845e2547288b4911e29266d2e
nelisriebezos <73901597+nelisriebezos@users.noreply.github.com>
2022-10-04 10:12:48 +0200

    Update PatientController.java
    i implemented the patient controller

--- src/main/java/org/ibs/presentation/PatientController.java
+++ src/main/java/org/ibs/presentation/PatientController.java
@@ -1,11 +1,42 @@
 package org.ibs.presentation;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.ibs.application.IPatientService;
+import org.ibs.application.dto.PatientDTO;
+import org.ibs.application.dto.builder.PatientDTOMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/patient")
 @AllArgsConstructor
 public class PatientController {
+    private final IPatientService patientService;
+    private final PatientDTOMapper patientDTOMapper;
+
+    @GetMapping("/{id}")
+    public PatientDTO getPatientById(@PathVariable long id) throws Exception {
+        return patientDTOMapper.toDTO(patientService.getById(id));
+    }
+
+    @GetMapping
+    public List<PatientDTO> getAllPatient() throws Exception {
+        return patientDTOMapper.toMultipleDTO(patientService.getAll());
+    }
+
+    @PostMapping
+    public PatientDTO createPatient(@RequestBody PatientDTO patientDTO) throws Exception {
+        return patientDTOMapper.toDTO(patientService.persistPatient(patientDTOMapper.fromDTO(patientDTO)));
+    }
+
+    @PatchMapping
+    public PatientDTO updatePatient(@RequestBody PatientDTO patientDTO) throws Exception {
+        return patientDTOMapper.toDTO(patientService.persistPatient(patientDTOMapper.fromDTO(patientDTO)));
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deletePatient(@PathVariable long id) throws Exception {
+        return patientService.deletePatient(id);
+    }
 }
