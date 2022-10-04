216e8b165f97dd6a97e6437ec80f079fff8cd164
MorrisT011 <74653965+MorrisT011@users.noreply.github.com>
2022-10-04 10:35:23 +0200

    Firebase app connection established

--- src/main/java/org/ibs/IBSApplication.java
+++ src/main/java/org/ibs/IBSApplication.java
@@ -1,13 +1,39 @@
 package org.ibs;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
 
 @SpringBootApplication
 public class IBSApplication
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException {
+        ClassLoader classLoader = IBSApplication.class.getClassLoader();
+
+
+        // Even kijken hoe dit precies werkt
+        File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+        FileInputStream serviceAccount = new FileInputStream(file.getAbsolutePath());
+
+//        FileInputStream serviceAccount =
+//                new FileInputStream("path/to/serviceAccountKey.json");
+
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+
+        FirebaseApp.initializeApp(options);
+
         SpringApplication.run(IBSApplication.class, args);
     }
 }
