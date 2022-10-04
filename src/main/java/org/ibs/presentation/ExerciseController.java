927f0e43e896d4f6dd1bd0fb9d702a25432d1e34
nelisriebezos <73901597+nelisriebezos@users.noreply.github.com>
2022-10-04 09:43:27 +0200

    Update ExerciseController.java
    I implemented the exercise controller.
    Note that most of these methods are really stupid, and will need more logic to make sure different DTOs are usable. This means that the service layer and DTO mappers most likely will need to be updated

--- src/main/java/org/ibs/presentation/ExerciseController.java
+++ src/main/java/org/ibs/presentation/ExerciseController.java
@@ -1,11 +1,42 @@
 package org.ibs.presentation;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.ibs.application.IExerciseService;
+import org.ibs.application.dto.ExerciseDTO;
+import org.ibs.application.dto.builder.ExerciseDTOMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/exercise")
 @AllArgsConstructor
 public class ExerciseController {
+    private final IExerciseService exerciseService;
+    private final ExerciseDTOMapper exerciseDTOMapper;
+
+    @GetMapping("/{id}")
+    public ExerciseDTO getExerciseById(@PathVariable long id) throws Exception {
+        return exerciseDTOMapper.toDTO(exerciseService.getById(id));
+    }
+
+    @GetMapping
+    public List<ExerciseDTO> getAllExercises() throws Exception {
+        return exerciseDTOMapper.toMultipleDTO(exerciseService.getAll());
+    }
+
+    @PostMapping
+    public ExerciseDTO createExercise(@RequestBody ExerciseDTO exerciseDTO) throws Exception {
+        return exerciseDTOMapper.toDTO(exerciseService.persistExercise(exerciseDTOMapper.fromDTO(exerciseDTO)));
+    }
+
+    @PatchMapping
+    public ExerciseDTO updateExercise(@RequestBody ExerciseDTO exerciseDTO) throws Exception {
+        return exerciseDTOMapper.toDTO(exerciseService.persistExercise(exerciseDTOMapper.fromDTO(exerciseDTO)));
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteExercise(@PathVariable long id) throws Exception {
+        return exerciseService.deleteCategory(id);
+    }
 }
