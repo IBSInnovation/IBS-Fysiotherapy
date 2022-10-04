3f9a88b8c51b893b38043de0e105940a7bf7c859
nelisriebezos <73901597+nelisriebezos@users.noreply.github.com>
2022-10-04 09:49:04 +0200

    Update MeasurementController.java
    I implemented the measurement controller

--- src/main/java/org/ibs/presentation/MeasurementController.java
+++ src/main/java/org/ibs/presentation/MeasurementController.java
@@ -1,11 +1,42 @@
 package org.ibs.presentation;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.ibs.application.IMeasurementService;
+import org.ibs.application.dto.MeasurementDTO;
+import org.ibs.application.dto.builder.MeasurementDTOMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/measurement")
 @AllArgsConstructor
 public class MeasurementController {
+    private final IMeasurementService measurementService;
+    private final MeasurementDTOMapper measurementDTOMapper;
+
+    @GetMapping("/{id}")
+    public MeasurementDTO getMeasurementById(@PathVariable long id) throws Exception {
+        return measurementDTOMapper.toDTO(measurementService.getById(id));
+    }
+
+    @GetMapping
+    public List<MeasurementDTO> getAllMeasurements() throws Exception {
+        return measurementDTOMapper.toMultipleDTO(measurementService.getAll());
+    }
+
+    @PostMapping
+    public MeasurementDTO createMeasurement(@RequestBody MeasurementDTO measurementDTO) throws Exception {
+        return measurementDTOMapper.toDTO(measurementService.persistMeasurement(measurementDTOMapper.fromDTO(measurementDTO)));
+    }
+
+    @PatchMapping
+    public MeasurementDTO updateMeasurement(@RequestBody MeasurementDTO measurementDTO) throws Exception {
+        return measurementDTOMapper.toDTO(measurementService.persistMeasurement(measurementDTOMapper.fromDTO(measurementDTO)));
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteMeasurement(@PathVariable long id) throws Exception {
+        return measurementService.deleteMeasurement(id);
+    }
 }
