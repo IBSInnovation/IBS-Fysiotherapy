64b5f103be2bec2705b3a42ec2ef46580514e0b3
nelisriebezos <73901597+nelisriebezos@users.noreply.github.com>
2022-10-04 10:16:22 +0200

    Update PhysiotherapistController.java
    I implemented the physiotherapist controller

--- src/main/java/org/ibs/presentation/PhysiotherapistController.java
+++ src/main/java/org/ibs/presentation/PhysiotherapistController.java
@@ -1,11 +1,42 @@
 package org.ibs.presentation;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.ibs.application.IPhysiotherapistService;
+import org.ibs.application.dto.PhysiotherapistDTO;
+import org.ibs.application.dto.builder.PhysiotherapistDTOMapper;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/physiotherapist")
 @AllArgsConstructor
 public class PhysiotherapistController {
+    private final IPhysiotherapistService physiotherapistService;
+    private final PhysiotherapistDTOMapper physiotherapistDTOMapper;
+
+    @GetMapping("/{id}")
+    public PhysiotherapistDTO getPatientById(@PathVariable long id) throws Exception {
+        return physiotherapistDTOMapper.toDTO(physiotherapistService.getById(id));
+    }
+
+    @GetMapping
+    public List<PhysiotherapistDTO> getAllPatient() throws Exception {
+        return physiotherapistDTOMapper.toMultipleDTO(physiotherapistService.getAll());
+    }
+
+    @PostMapping
+    public PhysiotherapistDTO createPatient(@RequestBody PhysiotherapistDTO physiotherapistDTO) throws Exception {
+        return physiotherapistDTOMapper.toDTO(physiotherapistService.persistPhysiotherapist(physiotherapistDTOMapper.fromDTO(physiotherapistDTO)));
+    }
+
+    @PatchMapping
+    public PhysiotherapistDTO updatePatient(@RequestBody PhysiotherapistDTO physiotherapistDTO) throws Exception {
+        return physiotherapistDTOMapper.toDTO(physiotherapistService.persistPhysiotherapist(physiotherapistDTOMapper.fromDTO(physiotherapistDTO)));
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deletePatient(@PathVariable long id) throws Exception {
+        return physiotherapistService.deletePhysiotherapist(id);
+    }
 }
