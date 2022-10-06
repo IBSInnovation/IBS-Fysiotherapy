3fc76907020b3145f2ee75a377f38556f01074d9
MorrisT011 <74653965+MorrisT011@users.noreply.github.com>
2022-10-06 17:42:02 +0200

    Changed return types to DTO in interface

--- src/main/java/org/ibs/application/IPhysiotherapistService.java
+++ src/main/java/org/ibs/application/IPhysiotherapistService.java
@@ -1,5 +1,6 @@
 package org.ibs.application;
 
+import org.ibs.application.dto.Physiotherapist.PhysiotherapistDTO;
 import org.ibs.domain.Physiotherapist;
 
 import java.util.List;
@@ -7,6 +8,6 @@ import java.util.List;
 public interface IPhysiotherapistService {
     Physiotherapist getById(String id) throws Exception;
     List<Physiotherapist> getAll() throws Exception;
-    Physiotherapist persistPhysiotherapist(Physiotherapist physiotherapist) throws Exception;
+    PhysiotherapistDTO persistPhysiotherapist(PhysiotherapistDTO physiotherapist) throws Exception;
     boolean deletePhysiotherapist(String id) throws Exception;
 }
