c56a78a77dc2c3bf9ca19f7cea94565cb4c39ada
MorrisT011 <74653965+MorrisT011@users.noreply.github.com>
2022-10-11 17:03:00 +0200

    Added no args constructor tag and physiotherapistId

--- src/main/java/org/ibs/application/dto/patientdto/GetPatient.java
+++ src/main/java/org/ibs/application/dto/patientdto/GetPatient.java
@@ -1,11 +1,15 @@
 package org.ibs.application.dto.patientdto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import org.ibs.utils.DTO;
 
 import java.util.Date;
 
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetPatient extends DTO {
     public String id;
     public String name;
@@ -14,5 +18,6 @@ public class GetPatient extends DTO {
     public Date dateOfBirth;
     public double height;
     public String email;
+    public String physiotherapistId;
 
 }
