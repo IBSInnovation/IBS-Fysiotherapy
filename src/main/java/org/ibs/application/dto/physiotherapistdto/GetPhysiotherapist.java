44b7632b899ec52087316d0035d58d73628dc8a3
MorrisT011 <74653965+MorrisT011@users.noreply.github.com>
2022-10-11 17:24:38 +0200

    Added no args constructor

--- src/main/java/org/ibs/application/dto/physiotherapistdto/GetPhysiotherapist.java
+++ src/main/java/org/ibs/application/dto/physiotherapistdto/GetPhysiotherapist.java
@@ -1,8 +1,12 @@
 package org.ibs.application.dto.physiotherapistdto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
 import org.ibs.utils.DTO;
 
+@NoArgsConstructor
+@AllArgsConstructor
 public class GetPhysiotherapist extends DTO {
     public String id;
     public String email;
