d1b050ab2fd6f48efc5a8ecef33a9300ee0f8054
MorrisT011 <74653965+MorrisT011@users.noreply.github.com>
2022-11-01 13:09:31 +0100

    Changed GetPatient Dto to fit new db-scheme

--- src/main/java/org/ibs/application/dto/patientdto/GetPatient.java
+++ src/main/java/org/ibs/application/dto/patientdto/GetPatient.java
@@ -12,8 +12,6 @@ public class GetPatient extends DTO {
     public String surName;
     public double weight;
     public Date dateOfBirth;
-    public double height;
     public String email;
-    public String physiotherapistId;
 
 }
