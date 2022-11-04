3ef1c01184812fa8012ee58efc3c97bcafd9eca6
MorrisT011 <74653965+MorrisT011@users.noreply.github.com>
2022-11-04 15:54:31 +0100

    Added new function to ExercisesService interface

--- src/main/java/org/ibs/application/IExerciseService.java
+++ src/main/java/org/ibs/application/IExerciseService.java
@@ -4,8 +4,13 @@ import org.ibs.application.dto.categorydto.SaveCategoryExercise;
 import org.ibs.application.dto.exercisedto.GetExercise;
 import org.ibs.application.dto.exercisedto.SaveExercise;
 
+import java.util.List;
+
 public interface IExerciseService {
     GetExercise getExerciseData(String id) throws Exception;
+
+    List<GetExercise> getExerciseDataByCategory(String categoryId) throws Exception;
+
     GetExercise saveExercise(SaveExercise saveExercise) throws Exception;
 
     SaveCategoryExercise saveExerciseToCategory(SaveCategoryExercise saveCategoryExercise) throws Exception;
