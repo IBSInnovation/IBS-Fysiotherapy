7195629b405ec2eb85473af26cc90b6bbc8b2c0a
MorrisT011 <74653965+MorrisT011@users.noreply.github.com>
2022-11-04 15:52:48 +0100

    Added NoArgs and AllArgs constructor tag

--- src/main/java/org/ibs/application/dto/categorydto/SaveCategoryExercise.java
+++ src/main/java/org/ibs/application/dto/categorydto/SaveCategoryExercise.java
@@ -1,5 +1,10 @@
 package org.ibs.application.dto.categorydto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
 public class SaveCategoryExercise {
     public String categoryId;
     public String exerciseId;
