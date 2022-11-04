429f85056af70019fac0cbe95e67ebaf97b11511
MorrisT011 <74653965+MorrisT011@users.noreply.github.com>
2022-11-04 16:20:35 +0100

    Added NoArgs and AllArgs constructor

--- src/main/java/org/ibs/application/dto/physiotherapistdto/SavePhysioPatient.java
+++ src/main/java/org/ibs/application/dto/physiotherapistdto/SavePhysioPatient.java
@@ -1,5 +1,10 @@
 package org.ibs.application.dto.physiotherapistdto;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class SavePhysioPatient {
     public String physioId;
     public String patientId;
